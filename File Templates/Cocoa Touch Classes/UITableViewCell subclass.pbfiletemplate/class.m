f9ba37e6244114b87dd63a2202c2a1d58432672c
Nick Partridge <nkpart@gmail.com>
2009-03-02 11:18:15 +1000

    Added copy of standard cocoa touch file templates. Stripped copyright and changed whitespace on NSObject

---
+++ File Templates/Cocoa Touch Classes/UITableViewCell subclass.pbfiletemplate/class.m
@@ -0,0 +1,34 @@
+//
+//  «FILENAME»
+//  «PROJECTNAME»
+//
+//  Created by «FULLUSERNAME» on «DATE».
+//  Copyright «YEAR» «ORGANIZATIONNAME». All rights reserved.
+//
+
+«OPTIONALHEADERIMPORTLINE»
+
+@implementation «FILEBASENAMEASIDENTIFIER»
+
+- (id)initWithFrame:(CGRect)frame reuseIdentifier:(NSString *)reuseIdentifier {
+    if (self = [super initWithFrame:frame reuseIdentifier:reuseIdentifier]) {
+        // Initialization code
+    }
+    return self;
+}
+
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+
+- (void)dealloc {
+    [super dealloc];
+}
+
+
+@end
---
+++ File Templates/Cocoa Touch Classes/UIView subclass.pbfiletemplate/class.m
@@ -0,0 +1,32 @@
+//
+//  «FILENAME»
+//  «PROJECTNAME»
+//
+//  Created by «FULLUSERNAME» on «DATE».
+//  Copyright «YEAR» «ORGANIZATIONNAME». All rights reserved.
+//
+
+«OPTIONALHEADERIMPORTLINE»
+
+@implementation «FILEBASENAMEASIDENTIFIER»
+
+
+- (id)initWithFrame:(CGRect)frame {
+    if (self = [super initWithFrame:frame]) {
+        // Initialization code
+    }
+    return self;
+}
+
+
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+
+
+- (void)dealloc {
+    [super dealloc];
+}
+
+
+@end
