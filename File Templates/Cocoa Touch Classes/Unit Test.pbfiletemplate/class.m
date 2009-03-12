5b24cb929e1101fda8638966de2fc3a2e6bbff15
Nick Partridge <nkpart@gmail.com>
2009-03-12 15:15:00 +1000

    Added unit test template

---
+++ File Templates/Cocoa Touch Classes/Unit Test.pbfiletemplate/class.m
@@ -0,0 +1,11 @@
+#import "GTMSenTestCase.h"
+
+@interface «FILEBASENAMEASIDENTIFIER» : GTMTestCase
+@end
+
+@implementation «FILEBASENAMEASIDENTIFIER»
+- (void)testYouShouldCopyThisClassAndPutARealTestInHere {
+    STAssertTrue(true, nil);
+}
+@end
+
